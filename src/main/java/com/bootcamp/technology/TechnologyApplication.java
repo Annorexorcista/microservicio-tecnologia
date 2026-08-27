0fcfb2e82d9f49c9858efe8f9173cace5367494e
Annorexorcista <jeremygarzonrios@hotmail.com>
2026-08-26 23:06:46 -0500

    Setup inicial

---
+++ src/main/java/com/bootcamp/technology/TechnologyApplication.java
@@ -0,0 +1,12 @@
+package com.bootcamp.technology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologyApplication.class, args);
+    }
+}
